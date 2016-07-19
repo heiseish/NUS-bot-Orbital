cdf8c729cdc17e20abe33d1452e0edf19e83804d
heiseish <dtrnggiang@gmail.com>
2016-07-19 21:25:21 +0800

    minor fix

--- Main/newBot.js
+++ Main/newBot.js
@@ -741,7 +741,7 @@ function writeEmail(address)
       				if (department){
       					fbMessage(sender,'Full Name: ' + fullNameOfProf + ', ' + designation + ', Department: ' + department + ', Email: ' + emaildecoded); 
       				} else {
-      					fbMessage(sender,'We cannot find the professor detail. Is it really ' + msg );
+      					fbMessage(sender,'We cannot find the professor detail. Is it really professor' + profName + '?');
       				} 
 
       				
