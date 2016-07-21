cf88005ec6c397763133654216d6a0a08c6c2e47
vitquay <vitquay1996@gmail.com>
2016-07-21 15:41:31 +0700

    insult

--- models/nusmod.js
+++ models/nusmod.js
@@ -168,7 +168,7 @@ findKey: function(string){
     intent = "thanks" 
   else if (string === "OH" || string === "EH" || string === "AH" || string == "OK")
     intent = "filler"
-  else if ((string.search("FUCK") != -1 || string.search("STUPID") != -1 || string.search("CHEAPO") != -1 || string.search("NOOB") != -1) && (string.search("YOU") != -1 || string.search("BOT") != -1))
+  else if ((string.search("FUCK") != -1 || string.search("STUPID") != -1 || string.search("CHEAPO") != -1 || string.search("NOOB") != -1) && (string.search("YOU") != -1 || string.search("BOT") != -1) || string.search("FUCK") == 0 || string.search("STUPID") == 0 || string.search("NOOB") == 0)
     intent = "insult"
   else if (string === "AWESOME" || string === "NICE" || string === "SMART" || ((string.search("AWESOME") != -1 || string.search("SMART") != -1 || string.search("NICE") != -1) && (string.search("BOT") != -1  || string.search("YOU") != -1)))
     intent = "commend";
