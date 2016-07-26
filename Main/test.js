a869a40418b3f6162920b1c5b13e81e5752719ba
heiseish <dtrnggiang@gmail.com>
2016-07-26 15:08:11 +0800

    add natural module

--- Main/test.js
+++ Main/test.js
@@ -1,39 +1,20 @@
-// var User = require('../models/model.js');
-// const express = require('express');
-// const request = require('request');
-// var graph = require('fbgraph');
-// var utility = require('../models/utility.js');
+const natural = require('natural');
 
+var corpus = ['something', 'soothing'];
+var spellcheck = new natural.Spellcheck(corpus);
 
+console.log(spellcheck.isCorrect('cat')); // false
+console.log(spellcheck.getCorrections('soemthing', 1)); // ['something']
+// console.log(spellcheck.getCorrections('soemthing', 2))// ['something', 'soothing']
 
-// graph.setVersion("2.6");
-// graph.setAccessToken('EAAQQyI1rDcUBAFzB4pMIYOBnQbFIuJvlmV68jqxithqRgoaYP90ZBZC629JezhLF57eSMew6PPtx8qv6DdelrLSAryPeZAIiGFz93ylWZB7yoJFQJZAQFZBxdZB5c57ZArcq0aJ5VS2qbokFE9nEsZBusdHsLumgHVmA34EzN2mQwawZDZD');
+natural.PorterStemmer.attach();
+console.log("I am waking up to the sounds of chainsaws".tokenizeAndStem());
+console.log("chainsaws".stem());
+console.log(natural.PorterStemmer.stem("I")); // stem a single word
 
-// User.findAll(function (err, users) {
-// 	if (err) console.error(err);
-// 	else {
-// 		if (users.length>0) {
-// 			for (var i=0; i<users.length; i++) {
-// 				console.log(users[i]);
-// 				graph.get(users[i].fbId, User.update(users[i].fbId,Uname,function(err){}){
-// 					var Uname = res.first_name + ' ' + res.last_name;
-// 					console.log(res);
+natural.LancasterStemmer.attach();
+console.log("i am waking up to the sounds of chainsaws".tokenizeAndStem());
+console.log("chainsaws".stem());
 
-
-// 				});
-// 			}
-// 		}
-// 	}
-// })
-// var utility = require('../models/utility.js');
-
-// utility.getUserName('1139314066115187').then(function(res){
-// 	console.log(res);
-// })
-var utility = require('../models/utility.js');
-var nus = require('../models/nusmod.js');
-
-
-nus.getModuleExam('MA1505').then(function(res){
-	console.log(utility.convertTime(res));
-})
+console.log(natural.JaroWinklerDistance("dixon","dicksonx"))
+console.log(natural.JaroWinklerDistance('not', 'same'));
